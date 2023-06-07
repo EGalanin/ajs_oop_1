e3517c478286454afed901d9ed69b80866941e50
EGalanin <99761893+EGalanin@users.noreply.github.com>
2023-06-07 17:37:27 +0300

    updated test

--- src/js/__test__/test.js
+++ src/js/__test__/test.js
@@ -1,22 +1,15 @@
 import Character from '../character';
 
 test('Short name', () => {
-  const result = () => { Character('N', 'Deamon', 50, 50); };
-  expect(result).toThrow();
+  expect(() => new Character('N', 'Deamon', 50, 50)).toThrow('Invalid length name');
 });
 
-// test('Short name2', () => {
-//   const result = () => { new Character('N', 'Deamon', 50, 50); };
-//   expect(result).toThrow('Invalid length name');
-// });
-
 test('Long name', () => {
-  const result = () => { Character('LOOOOONGNAME', 'Deamon', 50, 50); };
-  expect(result).toThrow();
+  expect(() => new Character('LOOOOONGNAME', 'Deamon', 50, 50)).toThrow('Invalid length name');
 });
 
 test('Invalid type', () => {
-  expect(() => { Character('Bob', 'FakeDeamon', 50, 50); }).toThrow();
+  expect(() => new Character('Bob', 'FakeDeamon', 50, 50)).toThrow('Invalid type');
 });
 
 // test('Invalid type', () => {
